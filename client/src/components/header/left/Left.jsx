bce12bee8a55d21bbb8574b2c2601c1f3281c52d
esrasen9 <esrasen860@gmail.com>
2022-02-06 21:39:53 +0300

    The home page has been linked to the logo.

--- client/src/components/header/left/Left.jsx
+++ client/src/components/header/left/Left.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Left.css";
 import logo from "../../../img/blogicon.png";
+import {Link} from "react-router-dom";
 
 const Left = () => {
     return (
-        <div className="left">
+        <Link to="/" className="left">
             <img className="left-img" src={logo} alt=""/>
-        </div>
+        </Link>
     );
 }
 
