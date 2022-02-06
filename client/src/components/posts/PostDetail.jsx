37f01820c75712c6e256d988de3faebf246fe148
esrasen9 <esrasen860@gmail.com>
2022-02-06 21:34:47 +0300

    The post's text has been truncated using CSS.

--- client/src/components/posts/PostDetail.jsx
+++ client/src/components/posts/PostDetail.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
 const PostDetail = () => {
-    const truncateString = (str, num) => {
-        if (num > str.length) {
-            return str;
-        }
-        return `${str.substring(0, num)}...`;
-    };
-    const content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, cumque!"
-
     return (
         <div className="post-details">
-            <h1 className="post-title">New Post</h1>
-            <div className="post-category-info">
-                <span className="post-category">Life</span>
-                <span className="post-category">Tech</span>
+            <div className="post-header">
+                <h1 className="post-title">New Post</h1>
+                <div className="post-category-info">
+                    <span className="post-category">Life</span>
+                    |
+                    <span className="post-category"> Tech</span>
+                </div>
             </div>
-            <p className="post-content">
-                {truncateString(content, 260)}
+            <p className="post-text">
+                Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+                Aliquam atque debitis dolore ipsa l
+                ibero maxime nihil quam qui quis rem!
+                Aperiam doloribus labore sapiente.
+                Animi blanditiis cum debitis doloribus
+                ducimus earum eligendi harum natus praesentium
+                quos, recusandae rem reprehenderit saepe unde
+                vitae! Corporis deserunt eaque exercitatione
+                m mollitia numquam rerum similique sunt ullam. Dolor, suscipit voluptatum. Doloremque esse eveniet, excepturi ipsum, libero magni maiores optio rem sit tempore ut voluptatem? Delectus dolorum ex explicabo id nam nihil, ratione sit voluptatem. Ab accusantium beatae commodi itaque laudantium maxime neque nostrum omnis quas quasi, quis, ratione sequi similique ullam veniam veritatis voluptas voluptatum.
             </p>
             <hr/>
             <span className="post-date">1 hour ago</span>
