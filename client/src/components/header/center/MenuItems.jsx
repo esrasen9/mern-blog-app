2fda82557d4fc23ae18c02256cde6ca3efd9c9fc
esrasen9 <esrasen860@gmail.com>
2022-02-11 16:20:36 +0300

    Update navLink classes

--- client/src/components/header/center/MenuItems.jsx
+++ client/src/components/header/center/MenuItems.jsx
@@ -5,9 +5,9 @@ import "./MenuItems.css";
 const MenuItems = () => {
     return (
         <div className="menu-items">
-            <NavLink exact activeClassName="active" className="menu-item" to={"/"}>Home</NavLink>
-            <NavLink activeClassName="active" className="menu-item" to={"/write"}>Write</NavLink>
-            <NavLink activeClassName="active" className="menu-item" to={"/about"}>About</NavLink>
+            <NavLink exact="true" className={(link)=>link.isActive ? "active menu-item" : "menu-item"} to={"/"}>Home</NavLink>
+            <NavLink className={(link)=>link.isActive ? "active menu-item" : "menu-item"} to={"/write"}>Write</NavLink>
+            <NavLink className={(link)=>link.isActive ? "active menu-item" : "menu-item"} to={"/about"}>About</NavLink>
         </div>
     );
 }
