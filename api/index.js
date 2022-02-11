115fc36e5557c225f3aea0a24954cd3c94e53901
esrasen9 <esrasen860@gmail.com>
2022-02-11 15:39:09 +0300

    Add file upload config

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
 const categoryRouter = require("./routes/categories");
-
+const multer = require("multer");
 dotenv.config();
 app.use(express.json());
 
@@ -18,6 +18,22 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL, {
     .catch((err) => {
         console.error(err.message);
     });
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null,"newFile.jpeg");
+    //req.body.fileName
+  }
+});
+
+const upload = multer({storage});
+app.post("/api/upload",upload.single("file"),(req, res)=>{
+  res.status(200).json("File has been uploaded successfully.");
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
