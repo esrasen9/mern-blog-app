abfbc74866bd4488ce49ead1fbe5f246712537b1
esrasen9 <esrasen860@gmail.com>
2022-02-14 00:24:09 +0300

    Handle logout

--- client/src/components/header/right/Icons.jsx
+++ client/src/components/header/right/Icons.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {BiLogIn, BiLogOut, BiSearch} from "react-icons/bi";
 import {Link} from "react-router-dom";
+import {useStateValue} from "../../../Context";
 const Icons = () => {
+    const {user,setUser} = useStateValue();
     return (
         <div className="icons">
             <button className="search-button">
                 <BiSearch size={50}/>
             </button>
-            {/*
-            <button className="icon-button">
-                <BiLogOut size={34}/>
-            <*/}
-            <Link className="sign-link" to="/sign">
-                <BiLogIn size={50}/>
-            </Link>
+            {
+                user ? (<button onClick={()=>setUser(null)} className="icon-button sign-link "><BiLogOut size={50}/></button>)
+                    : (<Link className="sign-link" to="/sign"><BiLogIn size={50}/></Link>)
+            }
         </div>
     );
 }
--- client/src/components/my-posts/MyPostsContainer.jsx
+++ client/src/components/my-posts/MyPostsContainer.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Post from "../post/Post";
 import "./MyPosts.css";
 import {FaMicroblog} from "react-icons/fa";
+import uuid from "react-uuid";
 const MyPostsContainer = ({posts}) => {
     return (
         <div className="my-posts-container">
             <h1 className="my-posts-title"> My Writings <FaMicroblog size={35}/></h1>
             <div className="my-posts">
                 {
-                    posts.map((post)=>(<Post post={post}/>))
+                    posts.map((post)=>(<Post key={uuid()} post={post}/>))
                 }
             </div>
         </div>
