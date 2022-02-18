ba80b02cbb28a07946b90b3971d2258b1b622a80
esrasen9 <esrasen860@gmail.com>
2022-02-18 19:42:22 +0300

    Styled

--- client/src/App.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import Provider from "./Context";
-test('renders learn react link', () => {
-});
